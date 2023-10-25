64cea7222dd837b9ec835d9ebc0392f248f0afa0
shamoel <shamoel.ahmad@gmail.com>
2023-10-25 15:44:36 +0530

    Removed unused code

--- src/main/java/org/example/interfaces/BookingManagerService.java
+++
@@ -1,8 +0,0 @@
-package org.example.interfaces;
-
-public interface BookingManagerService {
-
-    public boolean bookDevice(Device device, Person person);
-
-    public boolean returnDevice(Device device);
-}
--- src/main/java/org/example/interfaces/DeviceManagerService.java
+++
@@ -1,9 +0,0 @@
-package org.example.interfaces;
-
-import java.util.List;
-
-public interface DeviceManagerService {
-    List<Device> addDevice(Device device);
-    List<Device> removeDevice(Device device);
-    List<Device> listAllDevice();
-}
